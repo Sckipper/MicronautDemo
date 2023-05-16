5550db20a79d4adedbdbc0d4703a4869c41ce8aa
Sckipper <radutdaniel96@gmail.com>
2023-05-16 14:51:05 +0300

    bulk commit project init

---
+++ src/main/java/com/sckipper/mndemo/Application.java
@@ -0,0 +1,10 @@
+package com.sckipper.mndemo;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
